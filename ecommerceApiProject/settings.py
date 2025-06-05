9d63c2145b920fe68f1d66f33f5128f5e878cb4b
Bolubayo <abayomi.ogunmade@stu.cu.edu.ng>
2025-06-05 14:28:20 +0100

    sixth

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -35,7 +35,8 @@ DEBUG = True
 # ALLOWED_HOSTS = ["5c01-41-217-86-13.ngrok-free.app", "127.0.0.1"]
 # CSRF_TRUSTED_ORIGINS = ["https://5c01-41-217-86-13.ngrok-free.app"]
 
-ALLOWED_HOSTS = ["https://pyecommerce-production.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://pyecommerce-production.up.railway.app/"]
+
 ALLOWED_HOSTS = ["*"]
 
 
